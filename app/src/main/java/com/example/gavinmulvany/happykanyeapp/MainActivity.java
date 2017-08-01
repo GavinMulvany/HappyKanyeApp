1f674e06c5ed4b4351f8c3312d2d251ed23ab109
GavinMulvany <gavinmulvany96@gmail.com>
2017-08-01 20:09:43 +0100

    anim added

--- app/src/main/java/com/example/gavinmulvany/happykanyeapp/MainActivity.java
+++ app/src/main/java/com/example/gavinmulvany/happykanyeapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gavinmulvany.happykanyeapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -13,6 +16,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ImageView left, middle, right;
+
+    Button new_game;
 
     List<Integer> cups;
 
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         middle = (ImageView) findViewById(R.id.middle);
         right = (ImageView) findViewById(R.id.right);
 
+        new_game = (Button) findViewById(R.id.new_game);
+
 
         cups = new ArrayList<>();
         cups.add(107);
@@ -32,6 +39,30 @@ public class MainActivity extends AppCompatActivity {
         cups.add(407);
 
         Collections.shuffle(cups);
+
+        new_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Collections.shuffle(cups);
+
+
+
+                left.setImageResource(R.drawable.kanyecup);
+                middle.setImageResource(R.drawable.kanyecup);
+                right.setImageResource(R.drawable.kanyecup);
+
+                Animation anim_left = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left);
+                Animation anim_middle = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.middle);
+                Animation anim_right = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right);
+
+
+
+                left.startAnimation(anim_left);
+                middle.startAnimation(anim_middle);
+                right.startAnimation(anim_right);
+
+            }
+        });
 
         left.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,10 +74,10 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Guessed!", Toast.LENGTH_SHORT).show();
                 }
                 else if(cups.get(0) == 207){
-                    left.setImageResource(R.drawable.kh1);
+                    left.setImageResource(R.drawable.ks1);
                 }
                 else if(cups.get(0) == 407){
-                    left.setImageResource(R.drawable.kh1);
+                    left.setImageResource(R.drawable.ks1);
                 }
 
                 if(cups.get(1) == 107){
@@ -54,10 +85,10 @@ public class MainActivity extends AppCompatActivity {
 
                 }
                 else if(cups.get(1) == 207){
-                    middle.setImageResource(R.drawable.kh1);
+                    middle.setImageResource(R.drawable.ks1);
                 }
                 else if(cups.get(1) == 407){
-                    middle.setImageResource(R.drawable.kh1);
+                    middle.setImageResource(R.drawable.ks1);
                 }
 
                 if(cups.get(2) == 107){
@@ -65,10 +96,10 @@ public class MainActivity extends AppCompatActivity {
 
                 }
                 else if(cups.get(2) == 207){
-                    right.setImageResource(R.drawable.kh1);
+                    right.setImageResource(R.drawable.ks1);
                 }
                 else if(cups.get(2) == 407){
-                    right.setImageResource(R.drawable.kh1);
+                    right.setImageResource(R.drawable.ks1);
                 }
 
 
